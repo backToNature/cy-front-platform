1bf1b41df430da04b067ae1d37b56a2dd2b8040d
backToNature <backtonature@foxmail.com>
2015-12-13 20:33:52 +0800

    update

--- server/static/src/js/detail.js
+++ server/static/src/js/detail.js
@@ -1,13 +1,12 @@
 $(function () {
-    hljs.initHighlightingOnLoad();
     $(".markdown h2,h3,h4,h5,h6").each(function(i,item){
         var tag = $(item).get(0).localName;
         $(item).attr("id","wow"+i);
-        $("#toc").append('<a class="new'+tag+'" href="#wow'+i+'">'+$(this).text()+'</a></br>');
-        $(".newh2").css("margin-left", 0);
-        $(".newh3").css("margin-left", 20);
-        $(".newh4").css("margin-left", 40);
-        $(".newh5").css("margin-left", 60);
-        $(".newh6").css("margin-left", 80);
+        $("#toc").append('<li><a class="new'+tag+'" href="#wow'+i+'">'+$(this).text()+'</a></br></li>');
+        $(".newh2").css("padding-left", 20);
+        $(".newh3").css("padding-left", 40);
+        $(".newh4").css("padding-left", 60);
+        $(".newh5").css("padding-left", 80);
+        $(".newh6").css("padding-left", 100);
     });
 });
